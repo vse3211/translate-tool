dcf871e5dfd7974854eaebf3d68ba8e3fa800597
Londonist <52658840.m@gmail.com>
2025-09-24 01:00:33 +0500

    Add basic XML to JSON convertation to Stationeers.razor.cs

--- TranslateTool.Shared/Pages/Stationeers.razor.cs
+++ TranslateTool.Shared/Pages/Stationeers.razor.cs
@@ -1,7 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Xml;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 
 namespace TranslateTool.Shared.Pages;
 
 public partial class Stationeers : ComponentBase
 {
+    protected override async Task OnInitializedAsync()
+    {
+        XmlDocument sourceFile = new XmlDocument();
+        sourceFile.Load("source.xml");
+        string json = JsonConvert.SerializeXmlNode(sourceFile);
+    }
 }
